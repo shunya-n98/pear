d9727cfb97d190706a329d8c18f0dac3c3f2f540
shunya-n98 <57787741+shunya-n98@users.noreply.github.com>
2019-12-07 00:20:45 +0900

    kurochan

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
 //= require jquery
+//= require jquery.turbolinks
 //= require rails-ujs
+//= require activestorage
 //= require turbolinks
-//=require_tree .
+//= require_tree .
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,11 +1,18 @@
 $(function() {
-  $('.card').hover(
+  $('#walk').hover(
     ()=>{
-      $('.text-box').fadeIn();
+      $('#text-box-walk').fadeIn();
     },
     ()=> {
-      $('.text-box').fadeOut();
-    }
-  );
+      $('#text-box-walk').fadeOut();
+    },);
+
+  $('#del').hover(
+    ()=>{
+      $('#text-box-del').fadeIn();
+    },
+    ()=> {
+      $('#text-box-del').fadeOut();
+    });
 
 });
